5ff131dbbee10fbe94c9b3c2b9672b522e955754
majedul123 <majedul.islam@brainstation-23.com>
2025-05-08 13:13:42 +0600

    Add depdencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.majedul.steptracker"
-    compileSdk = 34
+    compileSdk = (libs.versions.projectCompileSdkVersion.get().toInt())
 
     defaultConfig {
         applicationId = "com.majedul.steptracker"
--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -34,10 +34,10 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 }
