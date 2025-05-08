d988c63485cb3d478086e6646a2c96d56eb401a6
majedul123 <majedul.islam@brainstation-23.com>
2025-05-08 14:57:32 +0600

    Add coil compose and map compose dependency

--- run/presentation/build.gradle.kts
+++ run/presentation/build.gradle.kts
@@ -33,11 +33,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose)
+    implementation(libs.google.maps.android.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.timber)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
