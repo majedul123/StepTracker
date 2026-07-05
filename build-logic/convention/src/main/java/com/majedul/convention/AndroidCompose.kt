7c5c9ae6eec57c80a4e90df6eca57a78cbc6d200
Majedul Islam <majedul.islam@brainstation-23.com>
2026-07-05 12:22:29 +0600

    11.1 Update agp for wear os.
    - Fix build issues

--- build-logic/convention/src/main/java/com/majedul/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/java/com/majedul/convention/AndroidCompose.kt
@@ -5,19 +5,14 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
 internal fun Project.configureAndroidCompose(
-    commonExtension: CommonExtension<*, *, *, *, *>
+    commonExtension: CommonExtension<*, *, *, *, *, *>
 ) {
     commonExtension.run {
         buildFeatures {
             compose = true
         }
 
-        composeOptions {
-            kotlinCompilerExtensionVersion = libs
-                .findVersion("composeCompiler")
-                .get()
-                .toString()
-        }
+        pluginManager.apply(libs.findPlugin("kotlin-compose").get().get().pluginId)
 
         dependencies {
             val bom = libs.findLibrary("androidx.compose.bom").get()
--- build-logic/convention/src/main/java/com/majedul/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/majedul/convention/BuildTypes.kt
@@ -10,7 +10,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 internal fun Project.configureBuildTypes(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
     extensionType: ExtensionType
 ) {
     commonExtension.run {
@@ -18,7 +18,7 @@ internal fun Project.configureBuildTypes(
             buildConfig = true
         }
 
-        val apiKey = gradleLocalProperties(rootDir).getProperty("API_KEY")
+        val apiKey = gradleLocalProperties(rootDir, providers).getProperty("API_KEY")
         when(extensionType) {
             ExtensionType.APPLICATION -> {
                 extensions.configure<ApplicationExtension> {
@@ -68,7 +68,7 @@ private fun BuildType.configureDebugBuildType(apiKey: String) {
 }
 
 private fun BuildType.configureReleaseBuildType(
-    commonExtension: CommonExtension<*, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
     apiKey: String
 ) {
     buildConfigField("String", "API_KEY", "\"$apiKey\"")
