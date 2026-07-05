d412c63b7d1beae177ad46f9428bd616ce57673d
Majedul Islam <majedul.islam@brainstation-23.com>
2026-07-05 11:34:05 +0600

    11.1 Add wear os module

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.androidDynamicFeature) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
